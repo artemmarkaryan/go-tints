2e6fc84afcbb15908878a115b9d6396872873e90
artemmarkaryan <artemandriy@gmail.com>
2021-04-19 01:08:20 +0300

    fixed exception handling in view

--- internal/views/product.go
+++ internal/views/product.go
@@ -1,25 +1,20 @@
 package views
 
 import (
-	"encoding/json"
 	"github.com/artemmarkaryan/gotints/internal/domain"
 	"log"
 	"net/http"
 )
 
 func GetProducts(w http.ResponseWriter, r *http.Request) {
-	products, err := domain.GetProducts(r.Context())
+	payload, err := domain.GetProducts(r.Context())
 	if err != nil {
-		log.Panic("cant get products: ", err)
+		payload = err.Error()
 	}
 
-	response, err := json.Marshal(products)
-	if err != nil {
-		log.Panic("cant serialize products: ", err)
-	}
+	_, err = w.Write([]byte(payload))
 
-	_, err = w.Write(response)
 	if err != nil {
-		log.Panic("cant write response: ", err)
+		log.Fatal("cant write response: ", err)
 	}
 }
